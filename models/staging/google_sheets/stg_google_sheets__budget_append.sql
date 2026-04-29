3cedae32b6d1db71df64a2a33170de83dc957631
Francisco Ortega Velasco <fvfotura@gmail.com>
2026-04-29 15:21:30 +0000

    Creación de incrementales append, merge y delete+insert

---
+++ models/staging/google_sheets/stg_google_sheets__budget_append.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='append'
+) }}
+
+WITH src_budget AS (
+    SELECT * FROM {{ source('GOOGLE_SHEETS', 'BUDGET') }}
+)
+
+SELECT
+    _row,
+    product_id,
+    quantity,
+    month,
+    _fivetran_synced AS date_load
+FROM src_budget
+
+{% if is_incremental() %}
+    WHERE _fivetran_synced > (SELECT MAX(date_load) FROM {{ this }})
+{% endif %}
---
+++ models/staging/google_sheets/stg_google_sheets__budget_merge.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='_row'
+) }}
+
+WITH src_budget AS (
+    SELECT * FROM {{ source('GOOGLE_SHEETS', 'BUDGET') }}
+)
+
+SELECT
+    _row,
+    product_id,
+    quantity,
+    month,
+    _fivetran_synced AS date_load
+FROM src_budget
+
+{% if is_incremental() %}
+    WHERE _fivetran_synced > (SELECT MAX(date_load) FROM {{ this }})
+{% endif %}
